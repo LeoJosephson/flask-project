9c3bcc0f25eb926882cca0193c2322fee938da7f
LeoJosephson <leonardojosephson24@gmail.com>
2022-12-14 10:48:56 -0300

    change blueprints registration

--- project/app.py
+++ project/app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
-from views.game.api import games
-from views.user.api import users
-from views.review.api import reviews
-from views.category.api import categories
-from extensions import db, ma
 from flasgger import Swagger
 import logging
 import os
+
+from views import init_blueprints
+from extensions import db, ma
 from config import DevelopmentConfig
 
+logging_format = '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+logging.basicConfig(format=logging_format, level=logging.DEBUG, datefmt='%d-%b-%y %H:%M:%S')
 MIGRATION_DIR = os.path.join('project', 'migrations')
 
 def create_app(config_class=DevelopmentConfig):
@@ -18,16 +18,10 @@ def create_app(config_class=DevelopmentConfig):
     swagger = Swagger(app, merge=True, template_file = 'docs/swagger.yaml')
     app.config.from_object(config_class)
     
-    app.register_blueprint(games)
-    app.register_blueprint(users)
-    app.register_blueprint(reviews)
-    app.register_blueprint(categories)
+    init_blueprints(app)
 
     db.init_app(app)
     ma.init_app(app)
     migrate = Migrate(app, db, directory="project/migrations", compare_type=True)
-
-    logging_format = '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
-    logging.basicConfig(format=logging_format, level=logging.DEBUG, datefmt='%d-%b-%y %H:%M:%S')
  
     return app
---
+++ project/views/__init__.py
@@ -0,0 +1,11 @@
+from .game.api import games
+from .user.api import users
+from .review.api import reviews
+from .category.api import categories
+
+def init_blueprints(app):
+    with app.app_context():
+        app.register_blueprint(games)
+        app.register_blueprint(users)
+        app.register_blueprint(reviews)
+        app.register_blueprint(categories)
